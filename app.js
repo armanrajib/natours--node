efac068c7f2b06a9d7efd1ddbe498a9dd37125b3
Arman Rajib <ahrajib6789@gmail.com>
2024-12-16 20:38:34 +0600

    Handle POST requests

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import fs from 'fs';
 import express from 'express';
 
 const app = express();
+app.use(express.json());
 
 const tours = JSON.parse(fs.readFileSync('./dev-data/data/tours-simple.json'));
 
@@ -12,6 +13,20 @@ app.get('/api/v1/tours', (req, res) => {
         data: {
             tours,
         },
+    });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+    const newId = tours[tours.length - 1].id + 1;
+    const newTour = { id: newId, ...req.body };
+    const updatedTours = [...tours, newTour];
+    fs.writeFile('./dev-data/data/tours-simple.json', JSON.stringify(updatedTours), (err) => {
+        res.status(201).json({
+            status: 'success',
+            data: {
+                tour: newTour,
+            },
+        });
     });
 });
 
