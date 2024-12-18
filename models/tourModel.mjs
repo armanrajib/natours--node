818af7826dd6b75d39b495b75e6fca4abace9d66
Arman Rajib <ahrajib6789@gmail.com>
2024-12-19 02:27:05 +0600

    Implement aggregation middleware (pre middleware)

--- models/tourModel.mjs
+++ models/tourModel.mjs
@@ -94,8 +94,14 @@ tourSchema.pre(/^find/, function (next) {
 });
 
 tourSchema.post(/^find/, function (docs, next) {
-    console.log(docs);
     console.log(`Query took ${Date.now() - this.start} milliseconds`);
+    next();
+});
+
+// AGGREGATION MIDDLEWARE
+tourSchema.pre('aggregate', function (next) {
+    this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
+    console.log(this.pipeline());
     next();
 });
 
