670a8aafb8743a1948c7caa54ccbc43c9618d22c
Arman Rajib <ahrajib6789@gmail.com>
2024-12-18 20:20:38 +0600

    Utilize aggregation pipeline (, , and )

--- routes/tourRoutes.mjs
+++ routes/tourRoutes.mjs
@@ -7,11 +7,14 @@ import {
     updateTour,
     deleteTour,
     aliasTopTours,
+    getTourStats,
 } from '../controllers/tourController.mjs';
 
 const router = express.Router();
 
 router.route('/top-5-cheap').get(aliasTopTours, getAllTours);
+
+router.route('/tour-stats').get(getTourStats);
 
 router.route('/').get(getAllTours).post(createTour);
 
