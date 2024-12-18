067528749d8e2a9133b5872b7cd34b77b1493966
Arman Rajib <ahrajib6789@gmail.com>
2024-12-18 20:44:17 +0600

    Utilize aggregation pipeline (, , , and )

--- routes/tourRoutes.mjs
+++ routes/tourRoutes.mjs
@@ -8,6 +8,7 @@ import {
     deleteTour,
     aliasTopTours,
     getTourStats,
+    getMonthlyPlan,
 } from '../controllers/tourController.mjs';
 
 const router = express.Router();
@@ -15,9 +16,9 @@ const router = express.Router();
 router.route('/top-5-cheap').get(aliasTopTours, getAllTours);
 
 router.route('/tour-stats').get(getTourStats);
+router.route('/monthly-plan/:year').get(getMonthlyPlan);
 
 router.route('/').get(getAllTours).post(createTour);
-
 router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
 
 export default router;
