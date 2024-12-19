d39ece706507e0d815b232b37d8712878912f597
Arman Rajib <ahrajib6789@gmail.com>
2024-12-19 20:43:11 +0600

    Implement handling errors during development vs production

--- controllers/errorController.mjs
+++ controllers/errorController.mjs
@@ -1,11 +1,42 @@
+const sendErrorDev = (err, req, res) => {
+    res.status(err.statusCode).json({
+        status: err.status,
+        error: err,
+        message: err.message,
+        stack: err.stack,
+    });
+};
+
+const sendErrorProd = (err, req, res) => {
+    // Operational, trusted error: send message to client
+    if (err.isOperational) {
+        res.status(err.statusCode).json({
+            status: err.status,
+            message: err.message,
+        });
+
+        // Programming or other unknown error: don't leak error details
+    } else {
+        // 1) Log error
+        console.error('ERROR 💥', err);
+
+        // 2) Send generic message
+        res.status(500).json({
+            status: 'error',
+            message: 'Something went very wrong!',
+        });
+    }
+};
+
 const globalErrorHandler = (err, req, res, next) => {
     err.statusCode = err.statusCode || 500;
     err.status = err.status || 'error';
 
-    res.status(err.statusCode).json({
-        status: err.status,
-        message: err.message,
-    });
+    if (process.env.NODE_ENV === 'development') {
+        sendErrorDev(err, req, res);
+    } else if (process.env.NODE_ENV === 'production') {
+        sendErrorProd(err, req, res);
+    }
 };
 
 export default globalErrorHandler;
